8bf08c03709d1c3094c12d66b99bb5f30aa894aa
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2021-09-09 11:05:35 +0200

    Fixed formatting

--- lib/widget/general/conditional_shower.dart
+++ lib/widget/general/conditional_shower.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 class ConditionalShower extends StatelessWidget {
   final bool isNull;
   final WidgetBuilder builder;
-  const ConditionalShower({required this.isNull, required this.builder, Key? key}) : super(key: key);
+  const ConditionalShower({
+    required this.isNull,
+    required this.builder,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return isNull ? const Center(child: CircularProgressIndicator()) : builder(context);
+    return isNull
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : builder(context);
   }
 }
