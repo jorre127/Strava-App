147670d377a37aeedefa5e7f548debf2d3a4a04c
Jordy De Jonghe <jordy.dejonghe@icapps.com>
2021-09-16 15:58:55 +0200

    Changed leaderboard so it now shows top performers in a table

--- lib/widget/club_detail/leader_board/leader_board.dart
+++ lib/widget/club_detail/leader_board/leader_board.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template/model/webservice/leader_board_spot/leader_board_spot.dart';
 import 'package:flutter_template/model/webservice/member_stats/member_stats.dart';
+import 'package:flutter_template/styles/theme_data.dart';
 import 'package:flutter_template/styles/theme_dimens.dart';
 import 'package:flutter_template/util/locale/localization.dart';
-import 'package:flutter_template/widget/club_detail/leader_board/leader_board_section.dart';
 import 'package:flutter_template/widget/general/section_card.dart';
 import 'package:flutter_template/widget/general/section_title.dart';
 import 'package:flutter_template/widget/provider/data_provider_widget.dart';
+import 'package:icapps_architecture/icapps_architecture.dart';
 
-class LeaderBoard extends StatelessWidget {
+class LeaderBoard extends StatefulWidget {
   final String title;
   final List<MemberStats> memberStats;
   final Localization localization;
@@ -19,60 +19,65 @@ class LeaderBoard extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
-  List<LeaderBoardSpot> getTopMembersDistance() {
-    final tempMemberStats = [...memberStats];
-    // ignore: cascade_invocations
-    tempMemberStats.sort(
-      (a, b) => b.totalDistance.compareTo(a.totalDistance),
-    );
-    final topThree = [
-      LeaderBoardSpot(
-        firstname: tempMemberStats[0].firstname,
-        lastname: tempMemberStats[0].lastname,
-        stat: tempMemberStats[0].totalDistance,
-      ),
-      LeaderBoardSpot(
-        firstname: tempMemberStats[1].firstname,
-        lastname: tempMemberStats[1].lastname,
-        stat: tempMemberStats[1].totalDistance,
-      ),
-      LeaderBoardSpot(
-        firstname: tempMemberStats[2].firstname,
-        lastname: tempMemberStats[2].lastname,
-        stat: tempMemberStats[2].totalDistance,
-      ),
-    ];
-    return topThree;
+  @override
+  _LeaderBoardState createState() => _LeaderBoardState();
+}
+
+class _LeaderBoardState extends State<LeaderBoard> {
+  var currentSortColumn = 0;
+  var isAscending = false;
+
+  void onSortDistance(int columnIndex, bool boolean) {
+    setState(() {
+      isAscending = !isAscending;
+    });
+    currentSortColumn = columnIndex;
+    if (isAscending) {
+      widget.memberStats.sort((memberA, memberB) => memberB.totalDistance.compareTo(memberA.totalDistance));
+    } else {
+      widget.memberStats.sort((memberA, memberB) => memberA.totalDistance.compareTo(memberB.totalDistance));
+    }
   }
 
-  List<LeaderBoardSpot> getTopMembersElevationGain() {
-    final tempMemberStats = [...memberStats];
-    // ignore: cascade_invocations
-    tempMemberStats.sort(
-      (a, b) => b.totalElevatiionGain.compareTo(a.totalElevatiionGain),
-    );
-    final topThree = [
-      LeaderBoardSpot(
-        firstname: tempMemberStats[0].firstname,
-        lastname: tempMemberStats[0].lastname,
-        stat: tempMemberStats[0].totalElevatiionGain,
-      ),
-      LeaderBoardSpot(
-        firstname: tempMemberStats[1].firstname,
-        lastname: tempMemberStats[1].lastname,
-        stat: tempMemberStats[1].totalElevatiionGain,
-      ),
-      LeaderBoardSpot(
-        firstname: tempMemberStats[2].firstname,
-        lastname: tempMemberStats[2].lastname,
-        stat: tempMemberStats[2].totalElevatiionGain,
-      ),
-    ];
-    return topThree;
+  void onSortElapsedTime(int columnIndex, bool boolean) {
+    setState(() {
+      isAscending = !isAscending;
+    });
+    currentSortColumn = columnIndex;
+    if (isAscending) {
+      widget.memberStats.sort((memberA, memberB) => memberB.totalElapsedTime.compareTo(memberA.totalElapsedTime));
+    } else {
+      widget.memberStats.sort((memberA, memberB) => memberA.totalElapsedTime.compareTo(memberB.totalElapsedTime));
+    }
+  }
+
+  void onSortMovingTime(int columnIndex, bool boolean) {
+    setState(() {
+      isAscending = !isAscending;
+    });
+    currentSortColumn = columnIndex;
+    if (isAscending) {
+      widget.memberStats.sort((memberA, memberB) => memberB.totalMovingTime.compareTo(memberA.totalMovingTime));
+    } else {
+      widget.memberStats.sort((memberA, memberB) => memberA.totalMovingTime.compareTo(memberB.totalMovingTime));
+    }
+  }
+
+  void onSortElevationGain(int columnIndex, bool boolean) {
+    setState(() {
+      isAscending = !isAscending;
+    });
+    currentSortColumn = columnIndex;
+    if (isAscending) {
+      widget.memberStats.sort((memberA, memberB) => memberB.totalElevatiionGain.compareTo(memberA.totalElevatiionGain));
+    } else {
+      widget.memberStats.sort((memberA, memberB) => memberA.totalElevatiionGain.compareTo(memberB.totalElevatiionGain));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+
     return DataProviderWidget(
       childBuilderTheme: (context, theme) => SectionCard(
         child: Padding(
@@ -80,23 +85,55 @@ class LeaderBoard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SectionTitle(title: title),
-              Row(
-                children: [
-                  LeaderBoardSection(
-                    memberStats: getTopMembersDistance(),
-                    title: localization.clubDetailTotalDistanceTitle,
-                    symbol: 'Km',
-                    conversion: 1000,
+              SectionTitle(title: widget.title),
+              SizedBox(
+                height: 300,
+                child: SingleChildScrollView(
+                  child: Expanded(
+                    child: SizedBox(
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: DataTable(
+                          columnSpacing: 30,
+                          sortColumnIndex: currentSortColumn,
+                          sortAscending: isAscending,
+                          dataTextStyle: theme.lightTextTheme.bodySmall,
+                          headingTextStyle: theme.lightTextTheme.bodySmall.copyWith(fontWeight: FontWeight.bold),
+                          columns: [
+                            const DataColumn(
+                              label: Text(''),
+                            ),
+                            DataColumn(
+                              onSort: onSortDistance,
+                              label: const Text('Total distance'),
+                            ),
+                            DataColumn(
+                              onSort: onSortElapsedTime,
+                              label: const Text('Total elapsed time'),
+                            ),
+                            DataColumn(
+                              onSort: onSortMovingTime,
+                              label: const Text('Total moving time'),
+                            ),
+                            DataColumn(
+                              onSort: onSortElevationGain,
+                              label: const Text('Total elevation gain'),
+                            ),
+                          ],
+                          rows: widget.memberStats
+                              .map((member) => DataRow(cells: [
+                                    DataCell(Text('${member.firstname} ${member.lastname}')),
+                                    DataCell(Text('${(member.totalDistance / 1000).toStringAsFixed(2)} Km')),
+                                    DataCell(Text('${((member.totalElapsedTime / 60) / 60).toStringAsFixed(2)} Hours')),
+                                    DataCell(Text('${((member.totalMovingTime / 60) / 60).toStringAsFixed(2)} Hours')),
+                                    DataCell(Text('${member.totalElevatiionGain} metres')),
+                                  ]))
+                              .toList(),
+                        ),
+                      ),
+                    ),
                   ),
-                  const Spacer(),
-                  LeaderBoardSection(
-                    memberStats: getTopMembersElevationGain(),
-                    title: localization.clubDetailTotalElevationGainTitle,
-                    symbol: 'm',
-                    conversion: 1,
-                  )
-                ],
+                ),
               )
             ],
           ),
